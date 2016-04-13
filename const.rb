c16c493a8a11fdc61bb599958f9e967fd3d97965
Andrey <andyvesel@ya.ru>
2016-04-13 16:07:19 +0300

    Add right solution

--- const.rb
+++ const.rb
@@ -1,26 +1,13 @@
 # 3 5 1
-arr = [-1,3,-4,5,1,-6,2,1]
-# arr = [0,0,0,3,0,-1,1]
+a = [-1,3,-4,5,1,-6,2,1]
+# a = [1,2,3,4,5]
+left_sum = 0 
+right_sum = a.inject(:+)
+  
+  a.each_with_index do |el, i|
 
-arr.each_with_index.reject {|el,i| i == 0}.each do |el, index|
- p arr[0..index+1]
-  if (arr[0..index-1].reduce :+) == (arr[index+1..arr.last].reduce :+)
-    puts el
-  else
-    puts "."
+    left_sum = left_sum + a[i - 1] if i > 0
+    right_sum -= el
+    
+    puts el if left_sum == right_sum
   end
-
-end
-
-# puts arr.reduce :+
-
-# arr.each_with_index.reject {|el,i| i == 0}.each do |el, index|
-
-#   index
-#   if arr[0..index].inject(0) {|sum, n| sum + n} == arr[index..arr.last].inject(0) {|sum, n| sum + n}
-#     p el
-#   else
-#     puts "."
-#   end
-
-# end
