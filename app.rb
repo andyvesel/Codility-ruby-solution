3bb4d2c9dfd21899eb76fb421740774578c7128c
Andrey Veselov <aveselov@redhat.com>
2016-06-07 15:04:58 +0200

    Rubocop that

--- app.rb
+++ app.rb
@@ -1,12 +1,11 @@
-a = [-1,3,-4,5,1,-6,2,1]
+a = [-1, 3, -4, 5, 1, -6, 2, 1]
 
-left_sum = 0 
+left_sum = 0
 right_sum = a.inject(:+)
-  
-  a.each_with_index do |el, i|
 
-    left_sum = left_sum + a[i - 1] if i > 0
-    right_sum -= el
-    
-    puts el if left_sum == right_sum
-  end
+a.each_with_index do |el, i|
+  left_sum += a[i - 1] if i > 0
+  right_sum -= el
+
+  puts el if left_sum == right_sum
+end
